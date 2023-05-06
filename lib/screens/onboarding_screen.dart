29edc5c5044225a12cdc97bc5a4a6cbf75cdebba
fahadmustjab <beingfahad786@gmail.com>
2023-05-06 09:56:16 +0500

    login signup completed

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,102 @@
+import 'package:event_management/screens/login_signup_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome to EMS!",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 27,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "Event Management System",
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: Image.asset('assets/onboardIcon.png'),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12, blurRadius: 5, spreadRadius: 2)
+                  ],
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      topLeft: Radius.circular(16))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    child: Text(
+                      "The social media platform designed to get you offline",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    child: Text(
+                      "EMS is an app where users can leverage their social network to create, discover, share, and monetize events or services.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      color: Colors.white,
+                      elevation: 2,
+                      onPressed: () {
+                        Get.to(() => const LoginScreen());
+                      },
+                      child: const Text(
+                        "Get Started",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff274560),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
